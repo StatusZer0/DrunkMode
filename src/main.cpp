a7e83f5c598c949bd62633722e07115b53e41b33
Zer0 <statuszero12@gmail.com>
2024-03-05 15:16:09 -0500

    Update main.cpp
    Modify the number generator to be a float instead of an int

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/CCScheduler.hpp>
 
 using namespace geode::prelude;
-
-#include <Geode/modify/CCScheduler.hpp>
 
 //set the main channel group for audio
 FMOD::ChannelGroup* masterGroup;
 
-int updateValue;
+int updateValue = 0;
 
 int randomInt = rand() % 30 + 85;   //generate a random number in range 85 - 115
-float randomNum = randomNum / 100; //divide it to make it 0.85 - 1.15
+float randomNum = static_cast<float>(randomInt) / 100; //divide it to make it 0.85 - 1.15
 
-//thanks to TheSillyDoggo and his QOLMod code to show me how to edit game speed
 class $modify (CCScheduler)
 {
     void update(float dt)
@@ -24,13 +22,14 @@ class $modify (CCScheduler)
 
         updateValue += 1;
 
-        if (updateValue >= 20)
+        if (updateValue >= 30)
         {
-            //update the value every 40 frames. I'm aware it can be buggy with higher frame rate but this is all I got for now
-            randomNum = rand() % 30 + 85;
-            randomNum = randomNum / 100;
+            //update the value every 30 frames. I'm aware it can be buggy with higher frame rate but this is all I got for now
+            randomInt = rand() % 30 + 85;
+            randomNum = static_cast<float>(randomInt) / 100;
             updateValue = 0;
         }
+        
         masterGroup->setPitch(randomNum);
                 
         CCScheduler::update(dt * randomNum);
